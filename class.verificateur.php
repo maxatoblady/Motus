8da8d5f4e5b7a9608a34d3ec2152b54068a78240
Simperfit <hamza@oblady.fr>
2014-02-24 16:27:42 +0100

    class verificateur

---
+++ class.verificateur.php
@@ -0,0 +1,36 @@
+<?php
+
+class verficateur {
+    
+var $motdico;
+
+    public function verificateur () {
+        $dico = new dictionnaire;
+        
+        $this->motdico = $dico->get_mot();
+        
+        
+    }
+    
+    public function verif_lettre($Smot) 
+    {
+        
+        
+       
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
